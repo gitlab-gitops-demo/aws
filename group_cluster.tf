20e9609fd7dfcae17dbcffceb1be71829582c8be
Brad Downey <bdowney@gitlab.com>
2019-10-02 15:12:04 -0700

    Change env scope of group cluster

--- group_cluster.tf
+++ group_cluster.tf
@@ -11,7 +11,7 @@ resource "gitlab_group_cluster" "aws_cluster" {
   group              = "${data.gitlab_group.gitops-demo-apps.id}"
   name               = "${module.eks.cluster_id}"
   domain             = "eks.gitops-demo.com"
-  environment_scope  = "old/"
+  environment_scope  = "eks/*"
   kubernetes_api_url = "${module.eks.cluster_endpoint}"
   kubernetes_token   = "${data.kubernetes_secret.gitlab-admin-token.data.token}"
   kubernetes_ca_cert = "${trimspace(base64decode(module.eks.cluster_certificate_authority_data))}"
