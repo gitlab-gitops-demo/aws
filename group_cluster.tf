7a26043811a6cae376ad63b8df819ed20315bf56
Brad Downey <bdowney@gitlab.com>
2019-12-02 09:10:14 -0800

    Update format to 0.12 Interpolation syntax

--- group_cluster.tf
+++ group_cluster.tf
@@ -6,12 +6,12 @@ provider "gitlab" {
   version = "~> 2.4.0"
 }
 resource "gitlab_group_cluster" "aws_cluster" {
-  group              = "${data.gitlab_group.gitops-demo-apps.id}"
-  name               = "${module.eks.cluster_id}"
+  group              = data.gitlab_group.gitops-demo-apps.id
+  name               = module.eks.cluster_id
   domain             = "eks.gitops-demo.com"
   environment_scope  = "eks/*"
-  kubernetes_api_url = "${module.eks.cluster_endpoint}"
-  kubernetes_token   = "${data.kubernetes_secret.gitlab-admin-token.data.token}"
-  kubernetes_ca_cert = "${trimspace(base64decode(module.eks.cluster_certificate_authority_data))}"
+  kubernetes_api_url = module.eks.cluster_endpoint
+  kubernetes_token   = data.kubernetes_secret.gitlab-admin-token.data.token
+  kubernetes_ca_cert = trimspace(base64decode(module.eks.cluster_certificate_authority_data))
 
 }
