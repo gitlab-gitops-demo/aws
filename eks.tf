2c32de492de9e8081bdb0bd66864025d485a49f1
Brad Downey <bdowney@gitlab.com>
2020-05-10 15:41:28 -0700

    Add a longer wait for cluster

--- eks.tf
+++ eks.tf
@@ -20,6 +20,7 @@ module "eks" {
       }]
     }
   ]
+  wait_for_cluster_cmd="until wget --no-check-certificate -O - -q $ENDPOINT/healthz >/dev/null; do sleep 30; done"
 }
 
 output "env-dynamic-url" {
