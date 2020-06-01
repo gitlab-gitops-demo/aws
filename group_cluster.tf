194b6a276184afdce8856127304d8de6dfe9143a
Brad Downey <bdowney@gitlab.com>
2020-06-01 12:14:42 -0700

    Use data element to search for cluster-management project.

--- group_cluster.tf
+++ group_cluster.tf
@@ -2,6 +2,14 @@ data "gitlab_group" "gitops-demo-apps" {
   full_path = "gitops-demo/apps"
 }
 
+data "gitlab_projects" "cluster-management-search" {
+  group_id            = data.gitlab_group.gitops-demo-apps.id
+  simple              = true
+  search              = "cluster-management"
+  per_page            = 1
+  max_queryable_pages = 1
+}
+
 provider "gitlab" {
   version = ">=2.9.0"
 }
@@ -13,5 +21,5 @@ resource "gitlab_group_cluster" "aws_cluster" {
   kubernetes_api_url    = module.eks.cluster_endpoint
   kubernetes_token      = data.kubernetes_secret.gitlab-admin-token.data.token
   kubernetes_ca_cert    = trimspace(base64decode(module.eks.cluster_certificate_authority_data))
-  management_project_id = "17106933"
+  management_project_id = data.gitlab_projects.cluster-management-search.projects.0.id
 }
