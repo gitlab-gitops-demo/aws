25f8172df70550ec30acbcbab20b37f1fb481cfa
Brad Downey <bdowney@gitlab.com>
2020-06-01 19:22:30 +0000

    Add management_cluster_id and update terraform provider version to 2.9.0

--- group_cluster.tf
+++ group_cluster.tf
@@ -2,33 +2,25 @@ data "gitlab_group" "gitops-demo-apps" {
   full_path = "gitops-demo/apps"
 }
 
+data "gitlab_projects" "cluster-management-search" {
+  # Returns a list of matching projects. limit to 1 result matching "cluster-management"
+  group_id            = data.gitlab_group.gitops-demo-apps.id
+  simple              = true
+  search              = "cluster-management"
+  per_page            = 1
+  max_queryable_pages = 1
+}
+
 provider "gitlab" {
-  version = "~> 2.4.0"
+  version = ">=2.9.0"
 }
 resource "gitlab_group_cluster" "aws_cluster" {
-  group              = data.gitlab_group.gitops-demo-apps.id
-  name               = module.eks.cluster_id
-  domain             = "eks.gitops-demo.com"
-  environment_scope  = "eks/*"
-  kubernetes_api_url = module.eks.cluster_endpoint
-  kubernetes_token   = data.kubernetes_secret.gitlab-admin-token.data.token
-  kubernetes_ca_cert = trimspace(base64decode(module.eks.cluster_certificate_authority_data))
-
-}
-
-# Work Around for lack of `management_project_id` in gitlab_group_cluster
-locals {
-  group_cluster_api_url = join("", ["https://gitlab.com/api/v4/", "groups/", gitlab_group_cluster.aws_cluster.group, "/clusters/", split(":", gitlab_group_cluster.aws_cluster.id)[1]])
-  curl_cmd = join("", ["curl -s --header \"Private-Token: $GITLAB_TOKEN\" ",
-    local.group_cluster_api_url,
-  " -H 'Content-Type:application/json' --request PUT --data '{\"management_project_id\":\"'$CLUSTER_MANAGEMENT_PROJECT_ID'\"}'"])
-}
-
-resource "null_resource" "gitlab-management-cluster-associate" {
-  triggers = { cluster_id = gitlab_group_cluster.aws_cluster.id }
-
-  provisioner "local-exec" {
-    command = local.curl_cmd
-  }
-  depends_on = [gitlab_group_cluster.aws_cluster]
+  group                 = data.gitlab_group.gitops-demo-apps.id
+  name                  = module.eks.cluster_id
+  domain                = "eks.gitops-demo.com"
+  environment_scope     = "eks/*"
+  kubernetes_api_url    = module.eks.cluster_endpoint
+  kubernetes_token      = data.kubernetes_secret.gitlab-admin-token.data.token
+  kubernetes_ca_cert    = trimspace(base64decode(module.eks.cluster_certificate_authority_data))
+  management_project_id = data.gitlab_projects.cluster-management-search.projects.0.id
 }
