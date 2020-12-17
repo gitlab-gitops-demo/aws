a607d79cd8818fcc9837a95c4199c8c97e95bfaf
Brad Downey <bdowney@gitlab.com>
2020-12-17 19:52:52 +0000

    Change cluster scope to *

--- group_cluster.tf
+++ group_cluster.tf
@@ -18,7 +18,7 @@ resource "gitlab_group_cluster" "aws_cluster" {
   group                 = data.gitlab_group.gitops-demo-apps.id
   name                  = module.eks.cluster_id
   domain                = "eks.gitops-demo.com"
-  environment_scope     = "eks/*"
+  environment_scope     = "*"
   kubernetes_api_url    = module.eks.cluster_endpoint
   kubernetes_token      = data.kubernetes_secret.gitlab-admin-token.data.token
   kubernetes_ca_cert    = trimspace(base64decode(module.eks.cluster_certificate_authority_data))
