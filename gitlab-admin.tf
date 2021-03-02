c35c2e006e0a19713e9a49fa9f565fdb746fa42e
Brad Downey <bdowney@gitlab.com>
2021-03-02 14:48:12 -0800

    Update for TF 0.14

--- gitlab-admin.tf
+++ gitlab-admin.tf
@@ -10,7 +10,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.my-cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.my-cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.my-auth.token
-  load_config_file       = false
 }
 
 resource "kubernetes_service_account" "gitlab-admin" {
