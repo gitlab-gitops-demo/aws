fd441ef5dad9858f833971f55755536457b1a4b1
Brad Downey <bdowney@gitlab.com>
2021-03-02 14:22:46 -0800

    Set disk to gp2 as default gp3 has a bug

--- eks.tf
+++ eks.tf
@@ -13,8 +13,9 @@ module "eks" {
 
   worker_groups = [
     {
-      instance_type = "m4.large"
-      asg_max_size  = 5
+      instance_type    = "m4.large"
+      asg_max_size     = 5
+      root_volume_type = "gp2" # Bug in default "gp3" https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1205
       tags = [{
         key                 = "Terraform"
         value               = "true"
