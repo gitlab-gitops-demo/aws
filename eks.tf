c477cc63dd8288b73c0e080b67bf6997ab461e06
Brad Downey <bdowney@gitlab.com>
2021-06-09 19:45:38 +0000

    Resolve "Terraform 1.0"

--- eks.tf
+++ eks.tf
@@ -1,8 +1,7 @@
 module "eks" {
   source           = "terraform-aws-modules/eks/aws"
-  version          = "14.0"
   cluster_name     = "gitops-demo-eks"
-  cluster_version  = "1.17"
+  cluster_version  = "1.19"
   subnets          = module.vpc.public_subnets
   write_kubeconfig = "false"
   tags = {
@@ -13,9 +12,8 @@ module "eks" {
 
   worker_groups = [
     {
-      instance_type    = "m4.large"
-      asg_max_size     = 5
-      root_volume_type = "gp2" # Bug in default "gp3" https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1205
+      instance_type = "m4.large"
+      asg_max_size  = 5
       tags = [{
         key                 = "Terraform"
         value               = "true"
