9b21769f5cfed4b1038b11bba2e39350571af6de
Brad Downey <bdowney@gitlab.com>
2022-07-11 14:20:48 -0700

    Update agent name and add token

--- gitlab-gitops-agent.tf
+++ gitlab-gitops-agent.tf
@@ -7,3 +7,10 @@ resource "gitlab_cluster_agent" "agent" {
   project = var.CI_PROJECT_ID
   name    = "agent"
 }
+
+resource "gitlab_cluster_agent_token" "agent" {
+  project     = gitlab_cluster_agent.agent.project
+  agent_id    = gitlab_cluster_agent.agent.agent_id
+  name        = "agent-token"
+  description = "agent token"
+}
